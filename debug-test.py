9c41e2a44675ab79cdeb63209231386133bf913a
Amit Patel <amitpatel60@gmail.com>
2025-09-10 14:23:35 -0400

    Start week 2

--- debug-test.py
+++ debug-test.py
@@ -1,5 +1,10 @@
+list_mul = ['2','2','2']
+list_mul*2
+print("answer:", list_mul*2)
 
+def function(var1=2, var2=4):
+    var2 = 6
+    var1 = 5
+    print(var1, var2)
 
-
-a = 0.5 * 5 * 10
-print(a)
+function(10, 20)
